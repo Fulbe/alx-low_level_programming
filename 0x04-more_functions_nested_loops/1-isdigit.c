1b2eab1a50da5a669cd7e92002f08bcb7aa678df
Fulbe <aminualiyu39@gmail.com>
2022-10-21 02:36:09 -0700

    alxse

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,10 @@
 *Return: returns 1 if it is uppercase , 0 if not
 */
 
-int _isdigit(int c)int _isdigit(int c)
+int _isdigit(int c)
 {
 if ((c >= 48) && (c <= 57))
 return (1);
 
 return (0);
-
+}
