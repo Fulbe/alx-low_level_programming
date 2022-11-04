790897377ab17fe0dbe72f074a57d3605fca12fc
Fulbe <aminualiyu39@gmail.com>
2022-11-04 08:38:07 -0700

    alxse

--- 0x09-static_libraries/1-args.c
+++ 0x09-static_libraries/1-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - Prints the number of arguments passed into thre function
- * @argc: The argument count
- * @argv: The argument vector
- * Return: Always 0
- */
-
+  *main - prints the number of arguments passed to it.
+  *@argc: number of arguemnts.
+  *@argv: array of size argc.
+  *
+  *Return: 0 on success.
+  */
 int main(int argc, char *argv[])
 {
+	int i;
 	(void)argv;
 
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc - 1; i++)
+	{}
 
+	printf("%d\n", i);
 	return (0);
 }
