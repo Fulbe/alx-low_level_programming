1347d41f894eb13d0ea233c07b086b171bc049c4
Fulbe <aminualiyu39@gmail.com>
2022-12-05 06:35:54 -0800

    alxse

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,36 @@
-#ifndef _HOLBERTON_
-#define _HOLBERTON_
+#include "main.h"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int s, t;
+	char *buf;
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+	if (!filename)
+		return (0);
 
-#endif
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	s = read(fd, buf, letters);
+	t = write(STDOUT_FILENO, buf, s);
+
+	close(fd);
+
+	free(buf);
+
+	return (t);
+}
